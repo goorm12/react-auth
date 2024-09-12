7a7f7f65601bee8dc6c4a3310d4f4da993832e09
binwoonki <vinwoonki98@gmail.com>
2024-09-12 16:50:08 +0900

    Auth -> SignUp으로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/auth" element={<SignUp />} />
+        <Route path="/SignUp" element={<SignUp />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </>
