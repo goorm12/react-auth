d69b84a01660ade53aa21d79ab780457a3ab8ff0
binwoonki <vinwoonki98@gmail.com>
2024-09-12 16:51:09 +0900

    SignUp 페이지 UI 기본 작업

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,14 @@
+import SignUpInput from "../components/SignUpInput";
+import Button from "../components/Button";
+import Header from "../components/Header";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <Header title={"회원가입"} leftChild={<Button text={"X"} />} />
+      <SignUpInput />
+    </div>
+  );
 };
 
 export default SignUp;
