f1064c964c141dcc552a5390ccf750e67941d457
binwoonki <vinwoonki98@gmail.com>
2024-09-12 15:51:05 +0900

    코드 수정

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "process.env.AIzaSyAzvRIFH3bFcxgj9ZktcS_MCY6V0HY0HRM",
-  authDomain: "process.env.react-auth-167ed.firebaseapp.com",
-  projectId: "process.env.react-auth-167ed",
-  storageBucket: "process.env.react-auth-167ed.appspot.com",
-  messagingSenderId: "process.env.513356712391",
-  appId: "process.env.1:513356712391:web:294d1ae466d244dec871e2",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const authService = getAuth(app);
