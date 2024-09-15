f8df4954959c0986f4fe52e72527907362b7946f
binwoonki <vinwoonki98@gmail.com>
2024-09-15 16:35:48 +0900

    input 컴포넌트를 따로 사용하지 않고 개별적으로 사용

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,29 +1,34 @@
 import "./SignIn.css";
-import Input from "./input";
-import Header from "./Header";
 import Button from "./Button";
-
+import { useNavigate } from "react-router-dom";
 const SignIn = () => {
+  const nav = useNavigate();
+
+  const HandleClick = () => {
+    nav("/signup", { replace: true });
+  };
+
   return (
     <div>
       <div className="SignIn">
         <div className="sign-in__id">
-          <Input
-            title={"아이디"}
+          <label htmlFor="input_id">아이디</label>
+          <input
             type={"text"}
             placeholder={"아이디를 입력해주세요"}
-            labelId={"input_id"}
+            id={"input_id"}
           />
         </div>
         <div className="sign-in__pw">
-          <Input
-            title={"비밀번호"}
+          <label htmlFor="input_pw">비밀번호</label>
+          <input
             type={"password"}
             placeholder={"비밀번호를 입력해주세요"}
-            labelId={"input_pw"}
+            id={"input_pw"}
           />
         </div>
         <Button text={"로그인"} bgc={"blue"} />
+        <Button text={"회원가입"} bgc={"blue"} onClick={HandleClick} />
       </div>
     </div>
   );
