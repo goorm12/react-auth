4d1853c272da64865fa2083724b40fee0496f296
binwoonki <vinwoonki98@gmail.com>
2024-09-15 14:57:43 +0900

    로그인 페이지 UI 완성

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,11 @@
+import SignIn from "../components/SignIn";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
 };
 
 export default Login;
