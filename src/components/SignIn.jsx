a7631b11758c4207d4ea147b87ee81bf6ce8b43d
binwoonki <vinwoonki98@gmail.com>
2024-09-15 20:46:12 +0900

    firebase를 이용한 로그인 구현하기

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,22 +1,49 @@
 import "./SignIn.css";
 import Button from "./Button";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { authService } from "../firebase/firebaseConfig";
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const nav = useNavigate();
 
   const HandleClick = () => {
     nav("/signup", { replace: true });
   };
 
+  const onLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const userSignIn = await signInWithEmailAndPassword(
+        authService,
+        email,
+        password
+      );
+      const user = userSignIn.user;
+      if (user) {
+        nav("/", { replace: true });
+      }
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error("Error Login user:", errorCode, errorMessage);
+    }
+  };
+
   return (
-    <div>
-      <div className="SignIn">
-        <div className="sign-in__id">
-          <label htmlFor="input_id">아이디</label>
+    <div className="SignIn">
+      <form onSubmit={onLogin}>
+        <div className="sign-in__email">
+          <label htmlFor="input_email">이메일</label>
           <input
-            type={"text"}
-            placeholder={"아이디를 입력해주세요"}
-            id={"input_id"}
+            type={"email"}
+            placeholder={"이메일을 입력해주세요"}
+            id={"input_email"}
+            autoComplete="off"
+            required
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="sign-in__pw">
@@ -25,11 +52,14 @@ const SignIn = () => {
             type={"password"}
             placeholder={"비밀번호를 입력해주세요"}
             id={"input_pw"}
+            autoComplete="off"
+            required
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <Button text={"로그인"} bgc={"blue"} />
+        <Button text={"로그인"} bgc={"blue"} type={"submit"} />
         <Button text={"회원가입"} bgc={"blue"} onClick={HandleClick} />
-      </div>
+      </form>
     </div>
   );
 };
