7f700a1913101e81f728488524a7aa61809ca728
binwoonki <vinwoonki98@gmail.com>
2024-09-15 21:31:16 +0900

    home 경로변경 및 error 메시지 추가

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,12 +1,15 @@
 import "./SignIn.css";
 import Button from "./Button";
+
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { authService } from "../firebase/firebaseConfig";
+
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const nav = useNavigate();
 
   const HandleClick = () => {
@@ -15,6 +18,7 @@ const SignIn = () => {
 
   const onLogin = async (e) => {
     e.preventDefault();
+
     try {
       const userSignIn = await signInWithEmailAndPassword(
         authService,
@@ -22,44 +26,56 @@ const SignIn = () => {
         password
       );
       const user = userSignIn.user;
+      console.log(user);
+
       if (user) {
-        nav("/", { replace: true });
+        nav("/home", { replace: true });
       }
     } catch (error) {
       const errorCode = error.code;
       const errorMessage = error.message;
       console.error("Error Login user:", errorCode, errorMessage);
+      if (errorMessage) {
+        setError("로그인 실패: 비밀번호를 확인해주세요!");
+      }
     }
   };
 
   return (
     <div className="SignIn">
-      <form onSubmit={onLogin}>
-        <div className="sign-in__email">
-          <label htmlFor="input_email">이메일</label>
-          <input
-            type={"email"}
-            placeholder={"이메일을 입력해주세요"}
-            id={"input_email"}
-            autoComplete="off"
-            required
-            onChange={(e) => setEmail(e.target.value)}
-          />
+      {error ? (
+        <div>
+          <div className="error-message">{error}</div>
+          <Button text={"로그인"} bgc={"blue"} onClick={() => setError("")} />
         </div>
-        <div className="sign-in__pw">
-          <label htmlFor="input_pw">비밀번호</label>
-          <input
-            type={"password"}
-            placeholder={"비밀번호를 입력해주세요"}
-            id={"input_pw"}
-            autoComplete="off"
-            required
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <Button text={"로그인"} bgc={"blue"} type={"submit"} />
-        <Button text={"회원가입"} bgc={"blue"} onClick={HandleClick} />
-      </form>
+      ) : (
+        <form onSubmit={onLogin}>
+          <div className="sign-in__email">
+            <label htmlFor="input_email">이메일</label>
+            <input
+              type={"email"}
+              placeholder={"이메일을 입력해주세요"}
+              id={"input_email"}
+              autoComplete="off"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="sign-in__pw">
+            <label htmlFor="input_pw">비밀번호</label>
+            <input
+              type={"password"}
+              placeholder={"비밀번호를 입력해주세요"}
+              id={"input_pw"}
+              autoComplete="off"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <Button text={"로그인"} bgc={"blue"} type={"submit"} />
+          <Button text={"회원가입"} bgc={"blue"} onClick={HandleClick} />
+        </form>
+      )}
     </div>
   );
 };
