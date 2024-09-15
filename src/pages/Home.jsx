e61757f7d4ce2cb34904a9bd279444d9b017af38
binwoonki <vinwoonki98@gmail.com>
2024-09-15 15:25:51 +0900

    링크를 연결하여 각 페이지로 이동할 수 있게 하기

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Link to="/login">로그인</Link>
+      <Link to="/signup">회원가입</Link>
+    </div>
+  );
 };
 
 export default Home;
