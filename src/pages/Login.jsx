a698c28f7c0482cc9f47d5136d263013daabf74a
binwoonki <vinwoonki98@gmail.com>
2024-09-15 15:45:59 +0900

    로그인 페이지 UI 변경 및 좌측 X 버튼 클릭 시 홈 화면으로 이동하기 기능 추가

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,23 @@
+import Button from "../components/Button";
+import Header from "../components/Header";
 import SignIn from "../components/SignIn";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const nav = useNavigate();
+
+  const handleClickHome = () => {
+    nav("/", { replace: true });
+  };
+
   return (
     <div>
+      <Header
+        title={"로그인"}
+        leftChild={
+          <Button text={"X"} bgc={"transparent"} onClick={handleClickHome} />
+        }
+      />
       <SignIn />
     </div>
   );
